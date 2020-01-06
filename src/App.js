7e04f58959b1bf69675f53e60075620d20e9c551
Alex <akuzin20@gmail.com>
2020-01-06 15:35:56 +0300

    v1 added Homepage, Aboutpage and navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import {Home} from "./pages/Home.page";
+import {About} from "./pages/About.page";
+import {Navbar} from "./components/Navbar.component";
 
 const App = () => {
     return (
-        <div className='container'>
-<h1>TEST</h1>
-        </div>
+        <BrowserRouter>
+            <Navbar/>
+            <div className='container pt-4'>
+                <Switch>
+                    <Route path='/' exact component={Home}/>
+                    <Route path='/about' component={About}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 };
 
